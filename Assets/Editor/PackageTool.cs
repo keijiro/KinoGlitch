02ea8a87cfd64bf5e243f6b589a8028aff98104a
Keijiro Takahashi <keijiro@ZX.local>
2015-07-03 21:54:30 +0900

    Unitypackage file added.

---
+++ Assets/Editor/PackageTool.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class PackageTool
+{
+    [MenuItem("Package/Update Package")]
+    static void UpdatePackage()
+    {
+        AssetDatabase.ExportPackage("Assets/Kino", "KinoGlitch.unitypackage", ExportPackageOptions.Recurse);
+    }
+}
